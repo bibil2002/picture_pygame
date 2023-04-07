0963f3ff14a69a620c4dbee6adafdcd576518823
Maks <maks.konanyhin@mail.ru>
2023-04-07 15:59:02 +0300

    Добавлен кирпичный_каркас_дома

--- picture.py
+++ picture.py
@@ -12,14 +12,24 @@ black = (0, 0, 0)
 yellow = (255, 255, 0)
 brown = (139, 69, 19)
 green = (128, 128, 0)
-
+balkon = (96,96,96)
 def full_picture():
     rect(screen, dark_gray, (0,0, 794,500))  # небо
 
-    def home():
-        pass
+    def home (x_home , y_home, width_home, height_home):
+        """
+
+        :param x_home: середины дома
+        :param y_home: середины дома
+        :param width_home: ширина только стен, без крыши и балкона
+        :param height_home:  без учета крыши и  труб(трубы вообще отдельной
+        функцией пойдут)
+        """
+        rect(screen, green,(x_home - width_home/2, y_home - height_home, width_home, height_home)) # кирпичи
 
 
+
+    home(794/2,1123/2,100,300)  # тестовый запуск
 
 full_picture()
 
